e7315a427f94eaf94c8f6343d189de5e95635122
tasmankingsley <tasman.kingsley@gmail.com>
2022-02-21 12:23:13 +1100

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo/dist/'
+  base: '/todo/dist/',
   plugins: [svelte()]
 })
