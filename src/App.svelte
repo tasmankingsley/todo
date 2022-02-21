71bd412ba9f1b92ccd3877a7b8a27d591e6085a1
tasmankingsley <tasman.kingsley@gmail.com>
2022-02-21 11:51:53 +1100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import Todo from "./lib/todo.svelte";
+</script>
+
+<main> 
+  <div>
+    <h1>tidy todo</h1>
+    <Todo/>
+  </div>
+</main>
+
+<style>
+  h1 {
+    font-size: 1.5rem;
+    font-weight: 300;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+  }
+
+
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/lib/todo.svelte
@@ -0,0 +1,78 @@
+<script>
+    import { fade, fly } from 'svelte/transition';
+
+    let new_item = "";
+
+    let todo_list = [];
+
+    function add_to_list() {
+        todo_list = [...todo_list, {text: new_item, status: false}];
+        new_item = "";
+    }
+
+    function remove_from_list(index) {
+        todo_list.splice(index, 1);
+        todo_list = todo_list;
+    }
+
+
+</script>
+
+<div>
+    <div class="input_div">
+        <input class="todo_input" bind:value={new_item} type="text" placeholder="something to do?">
+        <button on:click={add_to_list}>＋</button>
+    </div>
+    
+    {#each todo_list as item, index}
+        <div in:fly="{{ y: 200, duration: 1000 }}" out:fly="{{ x: -200, duration: 500 }}">
+            <input bind:checked={item.status} class="checkbox" type="checkbox">
+            <span class:checked={item.status} class="item">{item.text}</span>
+            <span on:click={() => remove_from_list(index)} class="remove_item">−</span>
+        </div>
+    {/each}
+</div>
+
+<style> 
+    .input_div {
+        display: flex;
+    }
+
+    .todo_input {
+        font-size: 1rem;
+        width: 100%;
+    }
+
+    button {
+        background-color: #7a879e9e;
+        color: #e9eff1;
+        font-size: 1rem;
+    }
+
+    button:hover {
+        background-color: #99d2d4;
+        color: #e9eff1;
+        cursor: pointer;        
+    }
+
+    input.checkbox {
+        width: 1rem;
+        height: 1rem;
+        vertical-align: middle;
+    }
+
+    .item {
+        line-height: 1rem;
+        vertical-align: middle;
+    }
+
+	.checked {
+        text-decoration: line-through;
+    }
+
+    .remove_item {
+        cursor: pointer;
+        float: right;
+    }
+
+</style>
